99edc6aacb73177dc60cb673a636631ed383fab3
De-Great Yartey <mail@degreat.co.uk>
2025-05-25 14:51:06 +0000

    match app styling

--- app/components/user-menu.tsx
+++ app/components/user-menu.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import React from "react";
-import { useLoaderData, useNavigate } from "react-router";
+import { Link, useLoaderData, useNavigate } from "react-router";
 import type { loader } from "~/routes/_index";
 import { InviteCard } from "./invite-card";
 
@@ -31,8 +31,8 @@ function UserMenu() {
 					{user.superUser && (
 						<div>
 							<header className="p-2 flex text-sm items-center justify-start">
-								<div className=" ms-3 flex gap-2 items-center text-secondary">
-									<div className="i-lucide-crown text-amber-600" />
+								<div className="bg-stone-200 dark:bg-neutral-800 px-2 rounded-lg flex gap-2 items-center text-secondary">
+									<div className="i-lucide-crown text-amber-500" />
 									Super User
 								</div>
 							</header>
@@ -40,13 +40,13 @@ function UserMenu() {
 							<hr className="dark:border-neutral-800" />
 						</div>
 					)}
-					<ul className="font-medium">
+					<ul className="font-medium p-1">
 						{user.superUser && (
 							<li>
 								<button
 									type="button"
 									onClick={() => setShowInvite(true)}
-									className="w-full flex gap-2 items-center py-1.5 px-5 bg-neutral-100 dark:bg-neutral-900 hover:bg-neutral-200 dark:hover:bg-neutral-800"
+									className="bg-transparent w-full flex gap-2 items-center py-1.5 px-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg"
 								>
 									<div className="i-solar-users-group-rounded-linear" />
 									Invite a Teammate
@@ -54,19 +54,19 @@ function UserMenu() {
 							</li>
 						)}
 						<li>
-							<a
-								href="/change-password"
-								className="w-full flex gap-2 items-center py-1.5 px-5 bg-neutral-100 dark:bg-neutral-900 hover:bg-neutral-200 dark:hover:bg-neutral-800"
+							<Link
+								to="/change-password"
+								className="w-full flex gap-2 items-center py-1.5 px-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg"
 							>
 								<div className="i-solar-lock-password-linear" />
 								Change Password
-							</a>
+							</Link>
 						</li>
 						<li>
 							<button
 								type="button"
 								onClick={handleLogout}
-								className="w-full flex gap-2 items-center py-1.5 px-5 text-red-600 bg-neutral-100 dark:bg-neutral-900 hover:bg-neutral-200 dark:hover:bg-neutral-800"
+								className="bg-transparent w-full flex gap-2 items-center py-1.5 px-2 text-red-500 hover:bg-red-600/5 rounded-lg"
 							>
 								<div className="i-solar-logout-2-outline" />
 								Logout
