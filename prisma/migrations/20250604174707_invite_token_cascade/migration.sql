21361882bcc84e24f63caf3a4d7ff7bc3c774b5d
De-Great Yartey <mail@degreat.co.uk>
2025-06-04 18:11:18 +0000

    compress migrations

--- prisma/migrations/20250604174707_invite_token_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "InviteToken" DROP CONSTRAINT "InviteToken_projectId_fkey";
-
--- AddForeignKey
-ALTER TABLE "InviteToken" ADD CONSTRAINT "InviteToken_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
