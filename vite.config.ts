55c7d8c8ac31101f8fde4a38e5bec7a642f50d7e
Ebenezer Arthur <arthurebenezer@aol.com>
2025-06-09 16:22:29 +0000

    just mess around

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [reactRouter(), UnoCSS(), tsconfigPaths()],
+	server: {
+		allowedHosts: ["a4cc-154-161-28-122.ngrok-free.app"],
+	},
 });
